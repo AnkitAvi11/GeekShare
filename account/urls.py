9831aeb88759713d20037832365d15fcf73a2048
Ankit Kumar <kumar.ankit383@gmail.com>
2020-09-06 21:24:30 +0530

    Nav

--- account/urls.py
+++ account/urls.py
@@ -9,13 +9,13 @@ urlpatterns = [
     path('signup/', views.register, name='signup'),
     path('setting/', views.setting, name='setting'),
 
-   #    password resetting views
-   path('password_reset/', auth_views.PasswordResetView.as_view(template_name='auth/password_reset.html'), name='password_reset'),
+    #    password resetting views
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='auth/password_reset.html'), name='password_reset'),
 
-   path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='auth/password_reset_done.html'), name='password_reset_done'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='auth/password_reset_done.html'), name='password_reset_done'),
 
-   path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='auth/reset.html'), name='password_reset_confirm'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='auth/reset.html'), name='password_reset_confirm'),
 
-   path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='auth/reset_done.html'), name='password_reset_complete')
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='auth/reset_done.html'), name='password_reset_complete')
 
 ]
