e121f0197cca3fc8b404e47d6c8b21635cf650b0
Ankit Kumar <kumar.ankit383@gmail.com>
2020-09-07 20:48:20 +0530

    User can now upload pdf or document files

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Note
+
+class NoteAdmin(admin.ModelAdmin) : 
+    list_display = ('id', 'title', 'description', 'note_file', 'user')
+    list_display_links = ('id', 'title')
+
+admin.site.register(Note, NoteAdmin)
--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from datetime import timedelta
 
-# Create your models here.
+class Note(models.Model) : 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(blank=False, null=False, max_length=300)
+    description = models.TextField(blank=True, null=True)
+    note_image = models.ImageField(upload_to='notes/%Y/%m/', blank=True, null=True)
+    note_file = models.FileField(upload_to='notes/%Y/%m/', blank=False, null=False)
+    created_on = models.DateTimeField(default=timezone.now())
+
+    def __str__(self):
+        return self.title
+
